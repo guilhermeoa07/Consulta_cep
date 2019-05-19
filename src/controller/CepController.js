44d71310459c6445db8c830f63c6120bb5e03dfe
Guilherme Oliveira <Guilherme.ol.an@gmail.com>
2019-05-19 17:38:28 -0300

    adcionando jwt

--- src/controller/CepController.js
+++ src/controller/CepController.js
@@ -1,13 +1,20 @@
 const router = require('express').Router();
 const Cep = require('../models/ceps');
+const auth = require('../middleware/auth');
 
-router.post('/', async (req, res) =>{
-    try{
+router.use(auth);
+
+router.post('/', async (req, res) => {
+    try {
         const cep = await Cep.create(req.body);
-        return res.send( { cep } )
-    } catch(err) {
+        return res.send({ cep })
+    } catch (err) {
         res.status(400).send({ erro: 'Erro ao Salvar o Cep' });
     }
 });
+
+router.get('/', async (req, res) => {
+    res.status(200).send({ Res: 'OK', id: req.userId});
+})
 
 module.exports = app => app.use('/cep', router);
--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,15 +1,39 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const authConfig = require('../config/auth')
 
-router.post('/', async (req, res) =>{
-    try{
+function generateToken(params = {}) {
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400,
+    });
+}
+
+router.post('/', async (req, res) => {
+    try {
         const user = await User.create(req.body);
         user.password = undefined;
-        return res.send( { user } )
-    } catch(err) {
-        res.status(400).send({ Erro : err } );
+        return res.status(200).send({ user, Token: generateToken({ id: user.id }) })
+    } catch (err) {
+        res.status(400).send({ Erro: err });
 
     }
 });
+
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+    if (!user)
+        return res.status(401).send({ Erro: 'Usuario não encontrado.' })
+
+    if (!await bcrypt.compare(password, user.password))
+        return res.status(401).send({ Erro: 'Senha Invalida.' });
+
+    user.password = undefined;
+
+    res.status(200).send({ Usuario: user.name, token: generateToken({ id: user.id }) });
+})
 
 module.exports = app => app.use('/user', router);
